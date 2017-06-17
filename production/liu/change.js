b0e425236f6b60a2988fa47b81a13c84d5c55476
DESKTOP-4KULG9A\13987 <yokocc255@163.com>
2017-06-17 22:52:26 +0800

    导航功能

---
+++ production/liu/change.js
@@ -0,0 +1,26 @@
+/**
+ * Created by 13987 on 2017/6/17.
+ */
+function hello() {
+
+    var place = $('.esriImpedanceCost').empty();
+    //
+    //
+    // $("<b>花费时间：</b>").appendTo(".esriResultsSummary");
+    var t = getKM().toString();
+    t = t.split('.')[0];
+    place.text(t + "分钟");
+    console.log(getKM().toString());
+}
+var lenth;
+function getKM() {
+    var text = $('.esriOtherCosts').text();
+    t = text.split(' ')[0];
+    lenth = parseFloat(t.substr(5));
+    return lenth * 1.7;
+}
+function startLoop() {
+    //重复执行某个方法
+    var t1 = window.setInterval(hello, 100);
+
+}
